7a0f91ade433fa28072cda6864148bf6d7da47fe
TatiParra21 <tatiparras21@gmail.com>
2026-03-19 10:41:30 -0500

    abcken

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,11 @@ import { Prisma } from "../generated/prisma/client";
 import { authMiddleWare } from './Middlewares/authMiddleWare'
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
+
 app.use(cookieParser())
 
 app.use("/file-bank",router)
